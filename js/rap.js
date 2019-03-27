a92f5d0b1e487e03cf3d1ff8f86074e81b1e4382
ikuma70 <kakeruww70@gmail.com>
2019-03-27 22:37:42 +0900

    キャンパスマップのレスポンシブのみ未実装

--- js/rap.js
+++ js/rap.js
@@ -1,23 +1,25 @@
 function sound()
 {
 	// 対象となるID名
-	var id = 'sound' ;
+	var sound = 'sound';
+	var video = 'bg-video';
 	// 初回以外だったら音声ファイルを巻き戻す
-	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	if(typeof(document.getElementById(sound).currentTime ) != 'undefined')
 	{
-		document.getElementById( id ).currentTime = 0;
+		document.getElementById(sound).currentTime = 0;
+	}
+	if(typeof(document.getElementById(video).currentTime ) != 'undefined')
+	{
+		document.getElementById(video).currentTime = 0;
 	}
 
 	// [ID:sound-file]の音声ファイルを再生[play()]する
-	document.getElementById( id ).play();
+	document.getElementById(sound).play();
+	document.getElementById(video).play();
 }
 
 function stop()
 {
-	var id = 'sound' ;
-	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
-	{
-		document.getElementById( id ).currentTime = 0;
-	}
-	document.getElementById( id ).pause();
+	document.getElementById('sound').pause();
+	document.getElementById('bg-video').pause();
 }
